05f33b19c25894301375ed39e228e16af63c44aa
Nestorov13 <nes7orov@gmail.com>
2022-12-02 19:21:50 +0200

    Initial Commit

---
+++ HundredDayChallenge/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HundredDayChallenge
+//
+//  Created by Borislav on 2.12.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HundredDayChallenge/HundredDayChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HundredDayChallengeApp.swift
+//  HundredDayChallenge
+//
+//  Created by Borislav on 2.12.22.
+//
+
+import SwiftUI
+
+@main
+struct HundredDayChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
